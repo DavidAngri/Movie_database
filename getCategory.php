0a17b9952c7c9233e8f3eb79fdd40224058ecd91
DPradeepa <71436787+DPradeepa@users.noreply.github.com>
2020-11-09 09:41:00 +0100

    Home Page Created

---
+++ getCategory.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'database.php';
+
+$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+
+if (!empty($_POST)) 
+{
+    $queryCat = "SELECT c.name, COUNT(c.name) movieCount
+                    FROM movies m
+                    JOIN movie_category mc ON m.movie_id = mc.movie_id
+                    JOIN categories c ON c.category_id = mc.category_id
+                    GROUP BY c.name";
+
+    $resultsCat = mysqli_query($conn, $queryCat);
+
+    // Retrieve Categories
+    $categories = mysqli_fetch_all($resultsCat, MYSQLI_ASSOC);
+
+    foreach ($categories as $category) {
+        echo '<a href="">' . $category['name'] . '(' . $category['movieCount'] . ')</a>';
+    }
+
+    
+}
