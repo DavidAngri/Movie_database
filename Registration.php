f9a39c8d6ba61e37d88def34bc36b34197f1923d
DPradeepa <71436787+DPradeepa@users.noreply.github.com>
2020-11-09 11:57:13 +0100

    updated form

--- Registration.php
+++ Registration.php
@@ -1,86 +1,123 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
+
+    <style>
+        .errors{
+            color: red;
+        }
+        form{
+            height: 500px;
+            width: 500px;
+            border: 2px solid blue;
+            margin-top: 10%;
+            margin-left: 20%;
+        }
+    </style>
 </head>
+
 <body>
     <?php
-        $firstname = '';
-        $lastname = '';
-        $nickname = '';
-        $email = '';
-        $password = '';
-        $confirm_password = '';
-        
-        if (isset($_POST['submitBtn']))
-        {
-            //  Retrieve form data and trim
-            $firstname = trim($_POST['firstname']);
-            $lastname = trim($_POST['lastname']);
-            $nickname = trim($_POST['nickname']);
-            $email = trim($_POST['email']);
-            $password = trim($_POST['password']);
-            $confirm_password = trim($_POST['confirm_password']);
-
-            // Encrypt the Password
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-            if (empty($firstname))
-                echo 'First Name is Mandatory!!!';
-            elseif (empty($lastname))
-                echo 'Last Name is Mandatory!!!';
-            elseif (empty($nickname))
-                echo 'Nick Name is Mandatory!!!';    
-            elseif (empty($email))
-                echo 'Email is Mandatory!!!';    
-            elseif (!filter_var($email, FILTER_VALIDATE_EMAIL))
-                echo 'Enter a Valid Email';
-            elseif (empty($password))
-                echo 'Password is Mandatory!!!';    
-            elseif (strlen($password) < 8)
-                echo 'Passsword must contain more than 8 Characters';
-            elseif (!$password == $confirm_password)    
-                echo 'Password and Confirm Password must Match';
-            else
-            {
-                require_once 'database.php';
-
-                $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-                $query = "SELECT * FROM users WHERE email = '$email' ";
-                $result = mysqli_query()
+    $firstname = '';
+    $lastname = '';
+    $nickname = '';
+    $email = '';
+    $password = '';
+    $confirm_password = '';
     
-                $query1 = "INSERT INTO users(firstname, lastname, nickname, email, password) 
-                            VALUES ('$firstname', '$lastname', '$email', '$hashedPassword')";
-                
-                $result1 = mysqli_query($conn, $query);
+    $errors = array();
 
+    if (isset($_POST['submitBtn'])) {
+        //  Retrieve form data and trim
+        $firstname = trim(htmlspecialchars($_POST['firstname']));
+        $lastname = trim(htmlspecialchars($_POST['lastname']));
+        $nickname = trim(htmlspecialchars($_POST['nickname']));
+        $email = trim(htmlspecialchars($_POST['email']));
+        $password = trim(htmlspecialchars($_POST['password']));
+        $confirm_password = trim(htmlspecialchars($_POST['confirm_password']));
 
-            }    
+        // Encrypt the Password
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
+        // Basic Validations for required fields
+
+        if (empty($firstname))
+            $errors['firstname'] = 'First Name is Mandatory!!!';
+        elseif (empty($lastname))
+            $errors['lastname'] = 'Last Name is Mandatory!!!';
+        elseif (empty($nickname))
+            $errors['nickname'] = 'Name is Mandatory!!!';
+        elseif (empty($email))
+            $errors['email'] = 'Email is Mandatory!!!';
+        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL))
+            $errors['email'] = 'Enter a Valid Email';
+        elseif (empty($password))
+            $errors['password'] = 'Password is Mandatory!!!';
+        elseif (strlen($password) < 8)
+            $errors['password'] = 'Passsword must contain more than 8 Characters';
+        elseif (!$password == $confirm_password)
+            $errors['confirm_password'] = 'Password and Confirm Password must Match';
+        elseif (count($errors == 0))
+        {
+  
+            require_once 'database.php';
+
+            $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+            $query = "SELECT * FROM users WHERE email = '$email' ";
+            $result = mysqli_query($query, $conn);
+
+            if(mysqli_num_rows($result) == 0)
+            {
+                $query_insert = "INSERT INTO users(firstname, lastname, nickname, email, password) 
+                VALUES ('$firstname', '$lastname', '$email', '$hashedPassword')";
+
+                $result_insert = mysqli_query($conn, $query);
+            }
+            else
+                echo "User with the email $email already Exists....";
+
+            
         }
+    }
 
     ?>
 
     <form action="" method="post">
         <div>
+            <!-- first Name -->
             <label for="firstname">First Name</label><br>
-            <input type="text" name="firstname" placeholder="Henri" value= '<?= $firstname ?>'><br>
+            <input type="text" name="firstname" placeholder="Henri" value='<?= $firstname ?>'>
+            <?php if(isset($errors['firstname'])) echo $errors['firstname']?><br>
+            <!-- last Name -->
             <label for="lastname">Last Name</label><br>
-            <input type="text" name="lastname" placeholder="Beck" value= '<?= $lastname ?>'><br>
+            <input type="text" name="lastname" placeholder="Beck" value='<?= $lastname ?>'>
+            <?php if(isset($errors['lastname'])) echo $errors['lastname']?><br>
+            <!-- Nick Name -->
             <label for="nickname">Nick Name</label><br>
-            <input type="text" name="nickname" placeholder="Alias Name" value= '<?= $nickname ?>'><br>
+            <input type="text" name="nickname" placeholder="Alias Name" value='<?= $nickname ?>'>
+            <?php if(isset($errors['nickname'])) echo $errors['nickname']?><br>
+            <!-- Email -->
             <label for="email">Email</label><br>
-            <input type="email" name="email" placeholder="henri.beck@gmail.com" value= '<?= $email ?>'><br>
+            <input type="text" name="email" placeholder="henri.beck@gmail.com" value='<?= $email ?>'>
+            <?php if(isset($errors['email'])) echo $errors['email']?><br>
+            <!-- Password -->
             <label for="password">Password</label><br>
-            <input type="password" name="password" placeholder="Enter your password" value= '<?= $password ?>'><br>
+            <input type="password" name="password" placeholder="Enter your password" value='<?= $password ?>'>
+            <?php if(isset($errors['password'])) echo $errors['password']?><br>
+            <!-- Confirm Password -->
             <label for="confirm_password">Confirm Password</label><br>
-            <input type="password" name="confirm_password" placeholder="Confirm your password" value= '<?= $confirm_password ?>'><br>
+            <input type="password" name="confirm_password" placeholder="Confirm your password" value='<?= $confirm_password ?>'>
+            <?php if(isset($errors['confirm_password'])) echo $errors['confirm_password']?><br>
+            <!-- Submit Button -->
             <input type="submit" name="submitBtn" value="Register"><br>
             Already have an account? <a href="">Login</a>
         </div>
     </form>
 </body>
+
 </html>
